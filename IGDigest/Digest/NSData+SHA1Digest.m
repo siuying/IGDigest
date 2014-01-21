6a3129b51049c03bb0f3e307432b3e25a9830e4f
J-P Teti <roboteti@me.com>
2014-01-21 18:42:28 -0500

    Added explicit casts to make it 64-bit compatible (without any warnings).

--- IGDigest/Digest/NSData+SHA1Digest.m
+++ IGDigest/Digest/NSData+SHA1Digest.m
@@ -14,7 +14,7 @@
 +(NSData *)SHA1Digest:(NSData *)input {
     unsigned char result[CC_SHA1_DIGEST_LENGTH];
     
-    if (CC_SHA1(input.bytes, input.length, result)) {
+    if (CC_SHA1(input.bytes, (CC_LONG)input.length, result)) {
         return [[NSData alloc] initWithBytes:result length:CC_SHA1_DIGEST_LENGTH];
     } else {
         return nil;
@@ -28,7 +28,7 @@
 +(NSString *)SHA1HexDigest:(NSData *)input {
     unsigned char result[CC_SHA1_DIGEST_LENGTH];
     
-    if (CC_SHA1(input.bytes, input.length, result)) {
+    if (CC_SHA1(input.bytes, (CC_LONG)input.length, result)) {
         NSMutableString *ret = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH*2];
         for (int i = 0; i<CC_SHA1_DIGEST_LENGTH; i++) {
             [ret appendFormat:@"%02x",result[i]];
